e1164245eae7e2f3fbd001373b23110d386db4a3
Kashish <kashishgoyal2019@u.northwestern.edu>
2022-07-06 14:54:32 -0400

    added interpreter logic

--- nodes.py
+++ nodes.py
@@ -4,6 +4,9 @@ from dataclasses import dataclass
 class NumberNode:
 	value: float
 
+	def eval(self):
+		return self.value
+
 	def __repr__(self):
 		return f"{self.value}"
 
@@ -11,6 +14,9 @@ class NumberNode:
 class AddNode:
 	node_a: any
 	node_b: any
+
+	def eval(self):
+		return self.node_a.eval() + self.node_b.eval()
 
 	def __repr__(self):
 		return f"({self.node_a}+{self.node_b})"
@@ -20,6 +26,9 @@ class SubtractNode:
 	node_a: any
 	node_b: any
 
+	def eval(self):
+		return self.node_a.eval() - self.node_b.eval()
+
 	def __repr__(self):
 		return f"({self.node_a}-{self.node_b})"
 
@@ -28,6 +37,9 @@ class MultiplyNode:
 	node_a: any
 	node_b: any
 
+	def eval(self):
+		return self.node_a.eval() * self.node_b.eval()
+
 	def __repr__(self):
 		return f"({self.node_a}*{self.node_b})"
 
@@ -35,6 +47,9 @@ class MultiplyNode:
 class DivideNode:
 	node_a: any
 	node_b: any
+
+	def eval(self):
+		return self.node_a.eval() / self.node_b.eval()
 
 	def __repr__(self):
 		return f"({self.node_a}/{self.node_b})"
